a0020b45c5ff1f047f02b08d10967afb2d1c58b5
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-03-04 12:27:40 +0100

    Updated description

--- NChordLib/Properties/AssemblyInfo.cs
+++ NChordLib/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NChordLib")]
-[assembly: AssemblyDescription("Source of NChordLib")]
+[assembly: AssemblyDescription("NChord Library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NChordLib")]
