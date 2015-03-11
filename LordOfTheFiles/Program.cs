7aa4d66a17a1a31a00e85bf3ee1cc24a03feacf0
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-03-11 08:34:59 +0100

    Added test data

--- LordOfTheFiles/Program.cs
+++ LordOfTheFiles/Program.cs
@@ -33,6 +33,7 @@ using System.Collections.Generic;
 using System.Text;
 
 using NChordLib;
+using System.Net;
 
 namespace TestNChord
 {
@@ -40,8 +41,30 @@ namespace TestNChord
     {
         static void Main(string[] args)
         {
-            ChordInstance instance = ChordServer.GetInstance(ChordServer.LocalNode);
-            instance.AddKey("hej");
+            int port = 8861;
+
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            string ip = ipHost.AddressList[ipHost.AddressList.Length - 2].ToString();
+
+            ChordServer.LocalNode = new ChordNode(ip, port);
+
+            System.Diagnostics.Debug.WriteLine(ChordServer.GetHash("hej"));
+
+            if (ChordServer.RegisterService(port))
+            {
+                ChordInstance instance = ChordServer.GetInstance(ChordServer.LocalNode);
+                instance.Join(null, ChordServer.LocalNode.Host, ChordServer.LocalNode.PortNumber);
+                //ChordInstance instance = ChordServer.GetInstance(ChordServer.LocalNode);
+
+                //instance.AddKey("hej");
+
+
+                //String test = instance.FindKey(ChordServer.GetHash("hej"));
+                //System.Diagnostics.Debug.WriteLine(test);
+            }
+
+            Console.ReadLine();
         }
     }
 }
